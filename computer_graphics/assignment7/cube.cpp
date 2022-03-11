535c98a1bb15cf8ff7522912a173164290cae1f7
gg-dev-05 <garvitgalgat@gmail.com>
2022-03-11 22:49:47 +0530

    assignment7

--- computer_graphics/assignment7/cube.cpp
+++ computer_graphics/assignment7/cube.cpp
@@ -1,84 +1,69 @@
-// 190001016
-// Garvit Galgat
-
 #include <bits/stdc++.h>
 #include <GL/glut.h>
 #include "imageio.h"
-using namespace std;
 
-#define WINDOW_WIDHT 500
-#define WINDOW_HEIGHT 500
-#define PERSPECTIVE 45.0
-#define NEARZ 0.0001  // nearest point for z-axis
-#define FARZ 50.0     // farthest point for z-axis
-
+#define PERSPECTIVE 60
+#define NEARZ 0.0001
+#define FARZ 50
+int imageWidthText;
+int imageHeightText;
 int window;
-GLfloat light_diffuse[] = {1.0, 1.0, 1.0, 1};
-GLfloat light_ambient[] = {0.0, 0.0, 0.0, 1.0};
-GLfloat light_specular[] = {1.0, 1.0, 1.0, 1.0};
-GLfloat light_position[] = {1.0, 1.0, 0.0, 0.0};
+GLfloat diffuseLight[] = {1.0, 1.0, 1.0, 1};
+GLfloat ambientLight[] = {0.0, 0.0, 0.0, 1.0};
+GLfloat specularLight[] = {1.0, 1.0, 1.0, 1.0};
+GLfloat positionLight[] = {1.0, 1.0, 0.0, 0.0};
+static GLuint textures[6];
+int yOrigin = -1, xOrigin = -1;
+float angDelta = 0.0f;
+float angDeltaZ = 0.0f;
+float angleX = 0;
+int angleY = 0, angleZ = 0;
 
-int light = 0;
-float red = 0;
-int sign = 1;
-float green = 0;
-float blue = 0;
-int anglex = 0, angley = 0, anglez = 0;  // rotation angles
-// images for texture maps for 6 faces of cube
-char maps[][12] = {"text.png", "texture.png", "texture.png", "text.png",
-                   "texture.png", "texture.png"};
-int texImageWidth;
-int texImageHeight;
-static GLuint texName[6];  // texture names
-GLubyte *makeTexImage(char *loadfile) {
+float lx = 0.0f, lz = 0.0f, ly = 0.0f;
+int lightSwitch = 0;
+
+char maps[][40] = {"textF.png", "textBR.png", "texture.png", "texture.png", "texture.png", "texture.png"};
+double deltaMovX = 0, deltaMovY = 0, deltaMovZ = 5;
+
+GLubyte *generateTextImage(char *loadfile) {
     int i, j, c, width, height;
     GLubyte *texImage;
-
-    /*
-      Only works for .png or .tif images.  NULL is returned if errors occurred.
-      loadImageRGA() is from imageio library downloaded from Internet.
-    */
     texImage = loadImageRGBA((char *)loadfile, &width, &height);
-    texImageWidth = width;
-    texImageHeight = height;
-    if (!texImage) printf("Unable to get texImage");
+    imageWidthText = width;
+    imageHeightText = height;
     return texImage;
 }
 
-void initTextures() {
-    // glClearColor(0.2, 0.2, 0.8, 0.0);
+void initialize(void) {
+    glClearColor(0, 0, 0, 1.0);
     glShadeModel(GL_FLAT);
-    glTranslatef(0.0, 0.0, -5.0);
-
     glEnable(GL_DEPTH_TEST);
 
     glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-    // texName is global
-    glGenTextures(6, texName);
+    glGenTextures(6, textures);
     for (int i = 0; i < 6; ++i) {
-        GLubyte *texImage = makeTexImage(maps[i]);
+        GLubyte *texImage = generateTextImage(maps[i]);
         if (!texImage) {
             printf("\nError reading %s \n", maps[i]);
             continue;
         }
-        glBindTexture(GL_TEXTURE_2D, texName[i]);  // now we work on texName
+        glBindTexture(GL_TEXTURE_2D, textures[i]);  // now we work on handles
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, texImageWidth,
-                     texImageHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, texImage);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, imageWidthText,
+                     imageHeightText, 0, GL_RGBA, GL_UNSIGNED_BYTE, texImage);
 
-        delete texImage;  // free memory holding texture image
+        delete texImage;
     }
 }
 
-void draw() {
+void draw(void) {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glEnable(GL_TEXTURE_2D);
     glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
-
-    int faces[6][4][3] = {
+    float face[6][4][3] = {
         {{-1, -1, 1}, {1, -1, 1}, {1, 1, 1}, {-1, 1, 1}},
         {{-1, 1, -1}, {1, 1, -1}, {1, -1, -1}, {-1, -1, -1}},
         {{1, -1, 1}, {1, -1, -1}, {1, 1, -1}, {1, 1, 1}},
@@ -86,60 +71,42 @@ void draw() {
         {{-1, 1, 1}, {1, 1, 1}, {1, 1, -1}, {-1, 1, -1}},
         {{-1, -1, 1}, {-1, -1, -1}, {1, -1, -1}, {1, -1, 1}}};
 
+    // to enable texture on only one face (front one)
     glEnable(GL_CULL_FACE);
     glCullFace(GL_BACK);
-
     glPushMatrix();
-    glRotatef(anglex, 1.0, 0.0, 0.0);  // rotate the cube along x-axis
-    glRotatef(angley, 0.0, 1.0, 0.0);  // rotate along y-axis
-    glRotatef(anglez, 0.0, 0.0, 1.0);  // rotate along y-axis
-
-    if (light) {
+    glRotatef(angleX + lx, 1.0, 0.0, 0.0);
+    if (lightSwitch == 1) {
         glEnable(GL_LIGHTING);
-        glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
-        glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
-        glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
-        glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+    } else if (lightSwitch == 2) {
+        glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuseLight);
+        glLightfv(GL_LIGHT0, GL_AMBIENT, ambientLight);
+        glLightfv(GL_LIGHT0, GL_SPECULAR, specularLight);
+        glLightfv(GL_LIGHT0, GL_POSITION, positionLight);
         glEnable(GL_LIGHT0);
         glEnable(GL_COLOR_MATERIAL);
         glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
-        glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, light_diffuse);
+        glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, diffuseLight);
     } else {
         glDisable(GL_LIGHTING);
         glDisable(GL_COLOR_MATERIAL);
         glDisable(GL_LIGHT0);
     }
+    glRotatef(angleY + lz, 0.0, 1.0, 0.0);
+    glRotatef(angleZ + ly, 0.0, 0.0, 1.0);
 
-    glLineWidth(5);
-    glBegin(GL_LINES);
-    glColor3f(1.0, 0.0, 0.0);
-    glVertex3f(-2.0, 0.0, 0.0);
-    glVertex3f(2.0, 0.0, 0.0);
-
-    glColor3f(0.0, 1.0, 0.0);
-    glVertex3f(0.0, -2.0, 0.0);
-    glVertex3f(0.0, 2.0, 0.0);
-
-    glColor3f(0.0, 0.0, 1.0);
-    glVertex3f(0.0, 0.0, -2.0);
-    glVertex3f(0.0, 0.0, 2.0);
-    glEnd();
-
-    glColor3f(red, green, blue);
-    for (int i = 0; i < 6; i++) {
-        glPushMatrix();
-        glBindTexture(GL_TEXTURE_2D, texName[i]);
+    for (int i = 0; i < 6; ++i) {
+        glBindTexture(GL_TEXTURE_2D, textures[i]);
         glBegin(GL_QUADS);
         glTexCoord2f(0.0, 0.0);
-        glVertex3iv(faces[i][0]);
-        glTexCoord2f(0.0, 1.0);
-        glVertex3iv(faces[i][1]);
-        glTexCoord2f(1.0, 1.0);
-        glVertex3iv(faces[i][2]);
+        glVertex3fv(face[i][0]);
         glTexCoord2f(1.0, 0.0);
-        glVertex3iv(faces[i][3]);
+        glVertex3fv(face[i][1]);
+        glTexCoord2f(1.0, 1.0);
+        glVertex3fv(face[i][2]);
+        glTexCoord2f(0.0, 1.0);
+        glVertex3fv(face[i][3]);
         glEnd();
-        glPopMatrix();
     }
 
     glPopMatrix();
@@ -147,102 +114,91 @@ void draw() {
     glDisable(GL_TEXTURE_2D);
     glutSwapBuffers();
 }
-// handle window resize match the aspect ratio
-void handleResize(int width, int height) {
-    glViewport(0, 0, width, height);
-
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    float aspectRatio = (double)width / (double)height;
-    gluPerspective(PERSPECTIVE, aspectRatio, NEARZ, FARZ);
-
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    glTranslatef(0, 0, -10);
-}
 
 void keyboard(unsigned char key, int x, int y) {
     switch (key) {
         case 's':
-            anglex = (anglex + 5) % 360;
+            lx = fmod((lx + 5), 360);
             break;
         case 'w':
-            anglex = (anglex - 5) % 360;
+            lx = fmod((lx - 5), 360);
             break;
         case 'd':
-            angley = (angley + 5) % 360;
+            ly = fmod((ly + 5), 360);
             break;
         case 'a':
-            angley = (angley - 5) % 360;
+            ly = fmod((ly - 5), 360);
             break;
         case 'q':
-            anglez = (anglez + 5) % 360;
+            lz = fmod((lz + 5), 360);
             break;
         case 'e':
-            anglez = (anglez - 5) % 360;
+            lz = fmod((lz - 5), 360);
             break;
         case 'l':
-            if (light)
-                light = 0;
-            else
-                light = 1;
-            break;
-        case 'r':
-            if (red >= 1)
-                sign = -1;
-            if (red <= 0)
-                sign = 1;
-            red = (red + sign * 0.1);
-            break;
-        case 'g':
-            if (green >= 1)
-                sign = -1;
-            if (green <= 0)
-                sign = 1;
-            green = (green + sign * 0.1);
-            break;
-        case 'b':
-            if (blue >= 1)
-                sign = -1;
-            if (blue <= 0)
-                sign = 1;
-            blue = (blue + sign * 0.1);
-            break;
-        case '.':
-            red = blue = green = 1;
+            lightSwitch = (lightSwitch + 1) % 3;
             break;
         case ' ':
-            anglex = 0;
-            angley = 0;
-            anglez = 0;
-            red = 0;
-            green = 0;
-            blue = 0;
+            lx = 0;
+            ly = 0;
+            lz = 0;
             break;
-        case 27: /* escape */
+        case 27:
             glutDestroyWindow(window);
             exit(0);
     }
     glutPostRedisplay();
 }
 
+void handleResize(int w, int h) {
+    glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(PERSPECTIVE, (GLfloat)w / (GLfloat)h, NEARZ, FARZ);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(deltaMovX, deltaMovY, deltaMovZ, 0, 0, 0, 0, 1, 0);
+}
+
+void mouseEvent(int x, int y) {
+    if (yOrigin >= 0) {
+        angDeltaZ = (x - xOrigin) * 0.3f;
+        angDelta = (y - yOrigin) * 0.3f;
+        lx = angDelta;
+        lz = angDeltaZ;
+    }
+}
+
+double modValue(double x, double y, double z) {
+    return sqrt(x * x + y * y + z * z);
+}
+
+void mousePress(int button, int state, int x, int y) {
+    if (button == 3 || button == 4) {
+        double valC = (1 + (button == 3 ? -1 : 1) * (double)1 / (10 * modValue(deltaMovX, deltaMovY, deltaMovZ)));
+        deltaMovX *= valC;
+        deltaMovY *= valC;
+        deltaMovZ *= valC;
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+        gluLookAt(deltaMovX, deltaMovY, deltaMovZ, 0, 0, 0, 0, 1, 0);
+    }
+}
+
 int main(int argc, char **argv) {
-    // Initialize GLUT
     glutInit(&argc, argv);
-    // GLUT_DOUBLE for better frames while interacting  with mouse
-    // GLUT_RGB for RGBA system
-    // GLUT_DEPTH for depth buffer
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-    glutInitWindowSize(WINDOW_WIDHT, WINDOW_HEIGHT);
-    // Create the window
-    window = glutCreateWindow("CUBE");
-    initTextures();
-    // Set handler functions
+    glutInitWindowSize(500, 500);
+    glutInitWindowPosition(100, 100);
+    window = glutCreateWindow(argv[0]);
+    initialize();
     glutDisplayFunc(draw);
     glutReshapeFunc(handleResize);
-    glutKeyboardFunc(keyboard);
     glutIdleFunc(draw);
 
-    glutMainLoop();  // start the main glut loop
+    glutKeyboardFunc(keyboard);
+    glutMouseFunc(mousePress);
+    glutMotionFunc(mouseEvent);
+    glutMainLoop();
     return 0;
 }
